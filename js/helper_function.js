f84d3c1affb7f5104c23ab2d6e2c8957d3c8392a
Shivam Miglani <shivam@wlan-145-94-177-249.wlan.tudelft.nl>
2017-12-19 15:56:19 +0100

    Interim

---
+++ js/helper_function.js
@@ -0,0 +1,201 @@
+
+//Change x and y location of each planet
+d3.timer(function() {
+    //Move the planet - DO NOT USE TRANSITION
+    d3.selectAll(".planet")
+        .attr("cx", locate("x"))
+        .attr("cy", locate("y"))
+        .attr("transform", function(d) {
+            return "rotate(" + (d.theta%360) + "," + d.x + "," + d.y + ")";
+        })
+    ;
+});
+
+//Calculate the new x or y position per planet
+function locate(coord) {
+    return function(d){
+        var k = 360 * d.major * d.minor  / ((d.Periodminutes*timeFactor) * resolution * speedUp);
+
+        for (var i = 0; i < resolution; i++) {
+                    d.theta += k / (d.r * d.r);
+                    d.r = d.major * (1 - d.Eccentricity * d.Eccentricity) / (1 - d.Eccentricity * Math.cos(toRadians(d.theta)));
+                    d.r = d.r * scalingFactor;
+        }
+        console.log(d.theta)
+        var x1 = d.r * Math.cos(toRadians(d.theta)) - (d.focus*scalingFactor);
+        console.log(x1);
+        var y1 = d.r * Math.sin(toRadians(d.theta));
+        console.log(y1)
+        if (d.theta > 360) {d.theta -= 360;}
+
+        if (coord == "x") {
+            //New x coordinates
+            newX = d.cx + x1 * Math.cos(toRadians(phi)) - y1 * Math.sin(toRadians(phi));
+            d.x = newX;
+            console.log(newX);
+            return newX;
+        } else if (coord == "y") {
+            newY =  d.cy + x1 * Math.sin(toRadians(phi)) + y1 * Math.cos(toRadians(phi));
+            d.y = newY;
+            console.log(newY);
+            return newY;
+        }
+    };
+}//function locate
+
+//Show the total orbit of the hovered over planet
+function showEllipse(d, i, opacity) {
+    var planet = i;
+    //console.log(d);
+    var duration = (opacity == 0) ? 2000 : 100; //If the opacity is zero slowly remove the orbit line
+
+    //Highlight the chosen planet
+    svg.selectAll(".planet")
+        .filter(function(d, i) {return i == planet;})
+        .transition().duration(duration)
+        .style("stroke-opacity", opacity * 1.25);
+
+    //Select the orbit with the same index as the planet
+    svg.selectAll(".orbit")
+        .filter(function(d, i) {return i == planet;})
+        .transition().duration(duration)
+        .style("stroke-opacity", opacity)
+        .style("fill-opacity", opacity/3);
+
+}
+
+//Turn degrees into radians
+function toRadians (angle) { return angle * (Math.PI / 180);}
+
+
+//Highlight the chosen planet and its orbit
+function highlight(planet, delayTime){
+
+    if(typeof(delayTime)==='undefined') delayTime = 0;
+    //if(typeof(tooltip)==='undefined') tooltip = true;
+    var time = 1000;
+
+    //Highlight the chosen planet
+    svg.selectAll(".planet")
+        .filter(function(d, i) {return i == planet;})
+        .transition().delay(700 * delayTime).duration(time)
+        .style("stroke-opacity", 1)
+        .style("opacity", 0.95)
+    //.transition()
+    //.each(function(d) {if (tooltip == true) {showTooltip(d);}})
+    ;
+
+    //Select the orbit with the same index as the planet
+    svg.selectAll(".orbit")
+        .filter(function(d, i) {return i == planet;})
+        .transition().delay(700 * delayTime).duration(time)
+        .style("stroke-opacity", 0.8)
+        .style("fill-opacity", 0.2);
+
+}
+
+
+//Function to bring opacity back of all planets
+function bringBack(opacity, delayTime){
+
+    if(typeof(delayTime)==='undefined') delayTime = 0;
+    var time = 500;
+
+    //Change opacity of all
+    svg.selectAll(".planet")
+        .transition().delay(700 * delayTime).duration(time)
+        .style("opacity", opacity);
+
+}
+
+
+//Dim all other planets (and orbits)
+function dimOne(planet, delayTime) {
+
+    if(typeof(delayTime)==='undefined') delayTime = 0;
+    var time = 500;
+
+    //Dim all other planets
+    svg.selectAll(".planet")
+        .filter(function(d, i) {return i == planet;})
+        .transition().delay(700 * delayTime).duration(time)
+        .style("stroke-opacity", 0)
+        .style("opacity", 0.1);
+
+    //Select the orbit with the same index as the planet
+    svg.selectAll(".orbit")
+        .filter(function(d, i) {return i == planet;})
+        .transition().delay(700 * delayTime).duration(time)
+        .style("stroke-opacity", 0)
+        .style("fill-opacity", 0);
+
+}//function dim
+
+
+//Dim all planets (and orbits)
+function dim(delayTime) {
+
+    if(typeof(delayTime)==='undefined') delayTime = 0;
+    var time = 1000;
+
+    //Dim all other planets
+    svg.selectAll(".planet")
+        .transition().delay(700 * delayTime).duration(time)
+        .style("stroke-opacity", 0)
+        .style("opacity", 0.1);
+
+    //Select the orbit with the same index as the planet
+    svg.selectAll(".orbit")
+        .transition().delay(700 * delayTime).duration(time)
+        .style("stroke-opacity", 0)
+        .style("fill-opacity", 0);
+
+}
+
+
+//Taken from http://bl.ocks.org/mbostock/7555321
+//Wraps SVG text
+function wrap(text, width) {
+    var text = d3.select(this[0][0]),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.4, // ems
+        y = text.attr("y"),
+        x = text.attr("x"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text.text(null).append("tspan").attr("x", x).attr("y", y).attr("dy", dy + "em");
+
+    while (word = words.pop()) {
+        line.push(word);
+        tspan.text(line.join(" "));
+        if (tspan.node().getComputedTextLength() > width) {
+            line.pop();
+            tspan.text(line.join(" "));
+            line = [word];
+            tspan = text.append("tspan").attr("x", x).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+        }
+    }
+};
+
+//Taken from https://groups.google.com/forum/#!msg/d3-js/WC_7Xi6VV50/j1HK0vIWI-EJ
+//Calls a function only after the total transition ends
+function endall(transition, callback) {
+    var n = 0;
+    transition
+        .each(function() { ++n; })
+        .each("end", function() { if (!--n) callback.apply(this, arguments); });
+}
+
+//Outline taken from http://stackoverflow.com/questions/16265123/resize-svg-when-window-is-resized-in-d3-js
+function updateWindow(){
+    x = (w.innerWidth || e.clientWidth || g.clientWidth) - 50;
+    y = (w.innerHeight|| e.clientHeight|| g.clientHeight) - 50;
+
+    svg.attr("width", x).attr("height", y);
+    d3.selectAll(".container").attr("transform", "translate(" + x/2 + "," + y/2 + ")");
+    d3.selectAll(".legendContainer").attr("transform", "translate(" + 30 + "," + (y - 90) + ")");
+    d3.select("#crazy").style("left", (x/2 - 112/2 + 6) + "px").style("top", (y/2 - 100) + "px");
+    //d3.selectAll(".introWrapper").attr("transform", "translate(" + -x/2 + "," + -y/2 + ")");
+}//updateWindow
--- js/main.js
+++ js/main.js
@@ -1,13 +1,97 @@
+//Width and Height of the SVG
+var
+    w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    x = (w.innerWidth || e.clientWidth || g.clientWidth) - 50,
+    y = (w.innerHeight|| e.clientHeight|| g.clientHeight) - 50;
+
+window.onresize = updateWindow;
+
+//Satellite orbit variables
+var resolution = 1,
+    speedUp = 400,
+    au = 149597871, //km
+    radiusEarth = 6371, //km
+    phi = 0, //rotation of ellipses
+    radiusSizer = 6, //Size increaser of radii of planets
+    planetOpacity = 0.6;
+scalingFactor = 0.01;
+timeFactor=1;
+
+
+
+//Create SVG
+var svg = d3.select("#planetarium").append("svg")
+    .attr("width", x)
+    .attr("height", y);
+
+
+
+//Create a container for everything with the centre in the middle
+var container = svg.append("g").attr("class","container")
+    .attr("transform", "translate(" + x/2 + "," + y/2 + ")")
 
 
 
 
-//loading data
-d3.csv("data.csv", function(data) {
-    data.forEach(function(d) {
-        d["Name of Satellite"]= +d["Name of Satellite"];
-        d["land area"] = +d["land area"];
+//Create Earth in the Middle - scaled to the orbits
+//Radius of Earth in these coordinates (taking into account size of circle inside image)
+var ImageWidth = radiusEarth*10/au * 3000 * (2.7/1.5);
+container.
+append("svg:image")
+    .attr("x", -ImageWidth)
+    .attr("y", -ImageWidth)
+    .attr("class", "sun")
+    .attr("xlink:href", "img/earth.png")
+    .attr("width", ImageWidth*2)
+    .attr("height", ImageWidth*2)
+    .attr("text-anchor", "middle");
+
+
+///////////////////////////////////////////////////////////////////////////
+/////////////////////////// Plot and move planets /////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+//Drawing a line for the orbit
+var orbitsContainer = container.append("g").attr("class","orbitsContainer");
+var orbits = orbitsContainer.selectAll("g.orbit")
+    .data(satellites).enter().append("ellipse")
+    .attr("class", "orbit")
+    .attr("cx", function(d) {return d.cx* scalingFactor;})
+    .attr("cy", function(d) {return d.cy;})
+    .attr("rx", function(d) {return d.major* scalingFactor;})
+    .attr("ry", function(d) {return d.minor* scalingFactor;})
+    .style("fill", "#3E5968")
+    .style("fill-opacity", 0)
+    .style("stroke", "white")
+    .style("stroke-opacity", 0);
+
+// Drawing the satellites
+var planetContainer = container.append("g").attr("class","planetContainer");
+var satellites = planetContainer.selectAll("g.planet")
+    .data(satellites).enter()
+    .append("circle")
+    .attr("class", "planet")
+    .attr("r", function(d) {return radiusSizer*0.6;})   //rScale(d.Radius);})
+    .attr("cx", function(d) {return d.major;})
+    .attr("cy", function(d) {return 0;})
+    .style("fill", "red")
+    .style("opacity", planetOpacity)
+    .style("stroke-opacity", 0)
+    .style("stroke-width", "3px")
+    .style("stroke", "yellow")
+    .on("mouseover", function(d, i) {
+        stopTooltip = false
+        showTooltip(d);
+        showEllipse(d, i, 0.8);
+    })
+    .on("mouseout", function(d, i) {
+        showEllipse(d, i, 0.8);
     });
-    console.log(data[0]);
-});
+
+
+
+
 
