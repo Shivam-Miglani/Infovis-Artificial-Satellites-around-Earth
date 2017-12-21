4301f4f0cf42921adc45f3885161a8bfb96b862c
galib360 <injamamul3.1416@gmail.com>
2017-12-21 01:33:47 +0100

    the selection now works with both the ellipses and scatter simultaneously, but working on the layout of the 2nd tab

--- js/ellipses.js
+++ js/ellipses.js
@@ -1,186 +1,200 @@
-d3.csv('data.csv', function (data) {
-    // Variables
+start();
+        
+        //var Country,Users;
+        
+        function select1(){
+            var Country = document.getElementById("Country").value;
+            var Users = document.getElementById("Users").value;
+            console.log("kuttamara")
+            if(Country=="All" && Users=="All"){start()}
+            else{
+            update1(Country , Users);
+            }
+        }
+        
+        
+        
+       
+    function update1(Country, Users){    
+      d3.csv('data.csv', function (data) {
+  // Variables
     console.log("data loaded!")
-    var svg = d3.select('#ellipsesvg');
-    svg.selectAll("g").remove();
-    var margin = { top: 50, right: 50, bottom: 50, left: 50 }
-    var h = 1000 - margin.top - margin.bottom
-    var w = 1000 - margin.left - margin.right
+  var svg = d3.select('#ellipsesvg');
+          svg.selectAll("g").remove();
+	var margin = { top: 50, right: 50, bottom: 50, left: 50 }
+	var h = 1000 - margin.top - margin.bottom
+	var w = 1000 - margin.left - margin.right
     //Country = document.getElementById("Country").value;
-
-
-    //var formatPercent = d3.format('.2%')
-    // Scales
-    //var colorScale = d3.scale.category20()
-
+    
+    
+	//var formatPercent = d3.format('.2%')
+	// Scales
+  //var colorScale = d3.scale.category20()
+    
     var colors = ["#1f77b4", "#2ca02c", "#d62728", "black"]
     var colorScale = d3.scale.ordinal()
-        .domain(["LEO", "MEO", "GEO", "Elliptical"])
-        //.domain(function (d) {return d.ClassOfOrbit})
-        .range(colors);
-
+                    .domain(["LEO", "MEO", "GEO", "Elliptical"])
+                    //.domain(function (d) {return d.ClassOfOrbit})
+                    .range(colors);
+    
     function translateFn(d){
-        return "rotate(" + d.Inclinationdegrees+ ", 500, 500)";
+        
+        return "rotate(" + d.Inclinationdegrees+ ", 500, 500)"; 
+        
     }
-
-    var xScale = d3.scale.linear()
-        .domain([
-            d3.min([0,d3.min(data,function (d) { return d.major})]),
-            d3.max([0,d3.max(data,function (d) { return d.major})])
-        ])
-        .range([0,100]);
-    var yScale = d3.scale.linear()
-        .domain([
-            d3.min([0,d3.min(data,function (d) { return d.minor })]),
-            d3.max([0,d3.max(data,function (d) { return d.minor })])
-        ])
-        .range([100,0]);
-    // SVG
-    svg = d3.select('#ellipsesvg')
-        .attr('height',h + margin.top + margin.bottom)
-        .attr('width',w + margin.left + margin.right)
+    
+  var xScale = d3.scale.linear()
+    .domain([
+    	d3.min([0,d3.min(data,function (d) { return d.major})]),
+    	d3.max([0,d3.max(data,function (d) { return d.major})])
+    	])
+    .range([0,100])
+  var yScale = d3.scale.linear()
+    .domain([
+    	d3.min([0,d3.min(data,function (d) { return d.minor })]),
+    	d3.max([0,d3.max(data,function (d) { return d.minor })])
+    	])
+    .range([100,0])
+	// SVG
+	   svg = d3.select('#ellipsesvg')
+	    .attr('height',h + margin.top + margin.bottom)
+	    .attr('width',w + margin.left + margin.right)
         .attr("fill", "#636363")
-        .append('g')
-        .attr('transform','translate(' + margin.left + ',' + margin.top + ')');
-
-
-    console.log(Country);
-    // Circles
-    var ellipse = svg.selectAll('ellipse')
-        .data(data)
-        .enter()
-        .append('ellipse')
-        .attr('cx', 500 )
-        .attr('cy', 500 )
-        .attr('rx', function(d) {return xScale(d.major*0.05)})
-        .attr('ry', function(d) {return yScale(d.minor*0.05)})
-        .attr('transform', translateFn)
-        .attr('stroke',function (d) { return colorScale(d.ClassofOrbit)})
-        .attr("fill", "none")
-        .attr('stroke-width',.5)
-        .on('mouseover', function () {
-            d3.select(this)
-                .transition()
-                .duration(500)
-                .attr("stroke","black")
-                .attr('stroke-width',5)
-        })
-        .on('mouseout', function () {
-            d3.select(this)
-                .transition()
-                .duration(500)
-                .attr('stroke-width',.5)
-                .attr('stroke',function (d) { return colorScale(d.ClassofOrbit)})
-        })
-        .append('title') // Tooltip
-        .text(function (d) { return d.NameofSatellite +
-            '\nEccentricity: ' + d.Eccentricity +
-            '\nInclination: ' + d.Inclinationdegrees +
-            '\nClass of orbit: ' + d.ClassofOrbit
-        })
-
-})
-
-
-function select(){
-    var Country = document.getElementById("Country").value;
-    var Users = document.getElementById("Users").value;
-    if(Country=="All" && Users=="All"){location.reload()}
-    console.log(Country);
+	    .append('g')
+	    .attr('transform','translate(' + margin.left + ',' + margin.top + ')');
+          
+          
+	
     console.log(Users);
-    update(Country , Users);
-}
-
-function update(Country, Users){
-    d3.csv('data.csv', function (data) {
-        // Variables
-        console.log("data loaded!")
-        var svg = d3.select('#ellipsesvg');
-        svg.selectAll("g").remove();
-        var margin = { top: 50, right: 50, bottom: 50, left: 50 }
-        var h = 1000 - margin.top - margin.bottom
-        var w = 1000 - margin.left - margin.right
-        //Country = document.getElementById("Country").value;
-
-
-        //var formatPercent = d3.format('.2%')
-        // Scales
-        //var colorScale = d3.scale.category20()
-
-        var colors = ["#1f77b4", "#2ca02c", "#d62728", "black"]
-        var colorScale = d3.scale.ordinal()
-            .domain(["LEO", "MEO", "GEO", "Elliptical"])
-            //.domain(function (d) {return d.ClassOfOrbit})
-            .range(colors);
-
-        function translateFn(d){
-
-            return "rotate(" + d.Inclinationdegrees+ ", 500, 500)";
-
+  // Circles
+  var ellipse = svg.selectAll('ellipse')
+      .data(data)
+      .enter()
+    .append('ellipse')
+      .attr('cx', 500 )
+      .attr('cy', 500 )
+      .attr('rx', function(d) {if (d.Country==Country && d.Users==Users){return xScale(d.major*0.05)}
+                              else if(Country=="All") {if(d.Users==Users){return xScale(d.major*0.05)}}
+                              //else if(Country=="All" && Users=="All"){return xScale(d.major*0.05)}
+                              else if(Users=="All") {if(d.Country==Country){return xScale(d.major*0.05)}}})
+      .attr('ry', function(d) {if (d.Country==Country && d.Users==Users){return yScale(d.minor*0.05)}
+                              else if(Country=="All"){if(d.Users==Users){return yScale(d.minor*0.05)}}
+                              //else if(Country=="All" && Users=="All"){return yScale(d.minor*0.05)}
+                              else if(Users=="All"){if(d.Country==Country){return yScale(d.minor*0.05)}}})
+      .attr('transform', translateFn)
+      .attr('stroke',function (d) { return colorScale(d.ClassofOrbit)})
+      .attr("fill", "none")
+      .attr('stroke-width',.5)
+      .on('mouseover', function () {
+        d3.select(this)
+          .transition()
+          .duration(500)
+          .attr("stroke","black")
+          .attr('stroke-width',5)
+      })
+      .on('mouseout', function () {
+        d3.select(this)
+          .transition()
+          .duration(500)
+          .attr('stroke-width',.5)
+          .attr('stroke',function (d) { return colorScale(d.ClassofOrbit)})
+      })
+    .append('title') // Tooltip
+      .text(function (d) { return d.NameofSatellite +
+                           '\nEccentricity: ' + d.Eccentricity +
+                           '\nInclination: ' + d.Inclinationdegrees +
+                         '\nClass of orbit: ' + d.ClassofOrbit
+                         })
+  
+})
+            
         }
+        function start(){
+        d3.csv('data.csv', function (data) {
+  // Variables
+    console.log("data loaded!")
 
-        var xScale = d3.scale.linear()
-            .domain([
-                d3.min([0,d3.min(data,function (d) { return d.major})]),
-                d3.max([0,d3.max(data,function (d) { return d.major})])
-            ])
-            .range([0,100])
-        var yScale = d3.scale.linear()
-            .domain([
-                d3.min([0,d3.min(data,function (d) { return d.minor })]),
-                d3.max([0,d3.max(data,function (d) { return d.minor })])
-            ])
-            .range([100,0])
-        // SVG
-        svg = d3.select('#ellipsesvg')
-            .attr('height',h + margin.top + margin.bottom)
-            .attr('width',w + margin.left + margin.right)
-            .attr("fill", "#636363")
-            .append('g')
-            .attr('transform','translate(' + margin.left + ',' + margin.top + ')');
-
-
-
-        console.log(Users);
-        // Circles
-        var ellipse = svg.selectAll('ellipse')
-            .data(data)
-            .enter()
-            .append('ellipse')
-            .attr('cx', 500 )
-            .attr('cy', 500 )
-            .attr('rx', function(d) {if (d.Country==Country && d.Users==Users){return xScale(d.major*0.05)}
-            else if(Country=="All") {if(d.Users==Users){return xScale(d.major*0.05)}}
-            else if(Users=="All") {if(d.Country==Country){return xScale(d.major*0.05)}}})
-            .attr('ry', function(d) {if (d.Country==Country && d.Users==Users){return yScale(d.minor*0.05)}
-            else if(Country=="All"){if(d.Users==Users){return yScale(d.minor*0.05)}}
-            else if(Users=="All"){if(d.Country==Country){return yScale(d.minor*0.05)}}})
-            .attr('transform', translateFn)
-            .attr('stroke',function (d) { return colorScale(d.ClassofOrbit)})
-            .attr("fill", "none")
-            .attr('stroke-width',.5)
-            .on('mouseover', function () {
-                d3.select(this)
-                    .transition()
-                    .duration(500)
-                    .attr("stroke","black")
-                    .attr('stroke-width',5)
-            })
-            .on('mouseout', function () {
-                d3.select(this)
-                    .transition()
-                    .duration(500)
-                    .attr('stroke-width',.5)
-                    .attr('stroke',function (d) { return colorScale(d.ClassofOrbit)})
-            })
-            .append('title') // Tooltip
-            .text(function (d) { return d.NameofSatellite +
-                '\nEccentricity: ' + d.Eccentricity +
-                '\nInclination: ' + d.Inclinationdegrees +
-                '\nClass of orbit: ' + d.ClassofOrbit
-            })
-
-    })
-
-}
-
+  var svg = d3.select('#ellipsesvg');
+          svg.selectAll("g").remove();
+	var margin = { top: 50, right: 50, bottom: 50, left: 50 }
+	var h = 1000 - margin.top - margin.bottom
+	var w = 1000 - margin.left - margin.right
+    //Country = document.getElementById("Country").value;
+    
+    
+	//var formatPercent = d3.format('.2%')
+	// Scales
+  //var colorScale = d3.scale.category20()
+    
+    var colors = ["#1f77b4", "#2ca02c", "#d62728", "black"]
+    var colorScale = d3.scale.ordinal()
+                    .domain(["LEO", "MEO", "GEO", "Elliptical"])
+                    //.domain(function (d) {return d.ClassOfOrbit})
+                    .range(colors);
+    
+    function translateFn(d){
+        
+        return "rotate(" + d.Inclinationdegrees+ ", 500, 500)"; 
+        
+    }
+    
+  var xScale = d3.scale.linear()
+    .domain([
+    	d3.min([0,d3.min(data,function (d) { return d.major})]),
+    	d3.max([0,d3.max(data,function (d) { return d.major})])
+    	])
+    .range([0,100])
+  var yScale = d3.scale.linear()
+    .domain([
+    	d3.min([0,d3.min(data,function (d) { return d.minor })]),
+    	d3.max([0,d3.max(data,function (d) { return d.minor })])
+    	])
+    .range([100,0])
+	// SVG
+	   svg = d3.select('#ellipsesvg')
+	    .attr('height',h + margin.top + margin.bottom)
+	    .attr('width',w + margin.left + margin.right)
+        .attr("fill", "#636363")
+	    .append('g')
+	    .attr('transform','translate(' + margin.left + ',' + margin.top + ')');
+          
+          
+	
+    console.log(Country);
+  // Circles
+  var ellipse = svg.selectAll('ellipse')
+      .data(data)
+      .enter()
+    .append('ellipse')
+      .attr('cx', 500 )
+      .attr('cy', 500 )
+      .attr('rx', function(d) {return xScale(d.major*0.05)})
+      .attr('ry', function(d) {return yScale(d.minor*0.05)})
+      .attr('transform', translateFn)
+      .attr('stroke',function (d) { return colorScale(d.ClassofOrbit)})
+      .attr("fill", "none")
+      .attr('stroke-width',.5)
+      .on('mouseover', function () {
+        d3.select(this)
+          .transition()
+          .duration(500)
+          .attr("stroke","black")
+          .attr('stroke-width',5)
+      })
+      .on('mouseout', function () {
+        d3.select(this)
+          .transition()
+          .duration(500)
+          .attr('stroke-width',.5)
+          .attr('stroke',function (d) { return colorScale(d.ClassOfOrbit)})
+      })
+    .append('title') // Tooltip
+      .text(function (d) { return d.NameofSatellite +
+                           '\nEccentricity: ' + d.Eccentricity +
+                           '\nInclination: ' + d.Inclinationdegrees +
+                         '\nClass of orbit: ' + d.ClassofOrbit
+                         })
+  
+})
+        
+        }
--- js/zoomablescatter.js
+++ js/zoomablescatter.js
@@ -1,18 +1,35 @@
+start();
+        
+var margin, width,height,colors, colorScale,x,y,tooltip,xAxis,yAxis,svg,clip,scatter,brush,idleTimeout,idleDelay, Country,Users;
 
+function select(){
+            var Country = document.getElementById("Country").value;
+            var Users = document.getElementById("Users").value;
+            
+            
+            if(Country=="All" && Users=="All"){start()}
+            else{
+            update(Country , Users);
+                select1();
+            }
+        }
+function update(Country,Users){
 d3.csv('data_ellipse.csv', function (data) {
 
+    svg = d3.select("#scattersvg")
+            .selectAll("g").remove();
 
-    var margin = { top: 20, right: 20, bottom: 30, left: 30 };
+     margin = { top: 20, right: 20, bottom: 30, left: 30 };
     width = 900 - margin.left - margin.right,
         height = 480 - margin.top - margin.bottom;
 
-    var colors = ["blue", "green", "red", "black"]
-    var colorScale = d3.scaleOrdinal()
+     colors = ["blue", "green", "red", "black"]
+     colorScale = d3.scaleOrdinal()
         .domain(["LEO", "MEO", "GEO", "Elliptical"])
         //.domain(function (d) {return d.ClassOfOrbit})
         .range(colors);
 
-    var x = d3.scaleLinear()
+     x = d3.scaleLinear()
         .domain([
             d3.min([0,d3.min(data,function (d) { return d.Inclination})]),
             d3.max([0,d3.max(data,function (d) { return d.Inclination})])
@@ -20,7 +37,7 @@ d3.csv('data_ellipse.csv', function (data) {
         .range([0, width])
         .nice();
 
-    var y = d3.scaleLinear()
+     y = d3.scaleLinear()
         .domain([
             d3.min([0,d3.min(data,function (d) { return d.ecc })]),
             d3.max([0,d3.max(data,function (d) { return d.ecc })])
@@ -28,13 +45,13 @@ d3.csv('data_ellipse.csv', function (data) {
         .range([height, 0]);
 
 
-    var tooltip = d3.select("#zoomableScatter").append("div")
+     tooltip = d3.select("#zoomableScatter").append("div")
         .attr("class", "tooltip")
         .style("opacity", 0);
 
 
 
-    var xAxis = d3.axisBottom(x).ticks(12),
+     xAxis = d3.axisBottom(x).ticks(12)
         yAxis = d3.axisLeft(y).ticks(12 * height / width);
 
 
@@ -43,13 +60,13 @@ d3.csv('data_ellipse.csv', function (data) {
     //  idleTimeout=100,
     //idleDelay = 350;
 
-    var svg = d3.select("#zoomableScatter").append('svg')
+     svg = d3.select("#scattersvg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var clip = svg.append("defs").append("svg:clipPath")
+     clip = svg.append("defs").append("svg:clipPath")
         .attr("id", "clip")
         .append("svg:rect")
         .attr("width", width )
@@ -65,11 +82,202 @@ d3.csv('data_ellipse.csv', function (data) {
 
 
 
-    var scatter = svg.append("g")
+     scatter = svg.append("g")
         .attr("id", "scatterplot")
         .attr("clip-path", "url(#clip)");
 
-    var brush = d3.brush().extent([[0, 0], [width, height]]).on("end", brushended),
+     brush = d3.brush().extent([[0, 0], [width, height]]).on("end", brushended),
+        //.on("start", function(){d3.select("scatter").call(brush)}),
+        idleTimeout,
+        idleDelay = 350;
+
+    scatter.append("g")
+        .attr("class", "brush")
+        .call(brush)
+
+    scatter.selectAll(".dot")
+        .data(data)
+        .enter().append("circle")
+        .attr("class", "dot")
+        .attr("r", 4)
+        .attr("cx", function (d) { if (d.Country==Country && d.Users==Users){return x(d.Inclination)}
+                              else if(Country=="All") {if(d.Users==Users){return x(d.Inclination)}}
+                              else if(Users=="All") {if(d.Country==Country){return x(d.Inclination)}}})
+        .attr("cy", function (d) { if (d.Country==Country && d.Users==Users){return y(d.ecc)}
+                              else if(Country=="All") {if(d.Users==Users){return y(d.ecc)}}
+                              else if(Users=="All") {if(d.Country==Country){return y(d.ecc)}}})
+        .attr("opacity", 0.5)
+        .style("fill", function (d) { return colorScale(d.ClassOfOrbit)})
+        .on('mouseover', function () {
+            //scatter.select(".brush").call(brush.move, null);
+            d3.select(this)
+                .transition()
+                .duration(500)
+                .attr('r',6)
+                .attr('stroke-width',.5)
+                .attr('stroke', 'black')
+        })
+        .on('mouseout', function () {
+            //scatter.select(".brush").call(brush);
+            d3.select(this)
+                .transition()
+                .duration(500)
+                .attr('r',4)
+
+        })
+        .append('title') // Tooltip
+        .text(function (d) { return d.NameofSatellite +
+            '\nEccentricity: ' + d.Eccentricity +
+            '\nInclination: ' + d.Inclination +
+            '\nClass of orbit: ' + d.ClassOfOrbit});
+
+
+
+
+    // x axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr('id', "axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("text")
+        .style("text-anchor", "end")
+        .attr("x", width)
+        .attr("y", height - 8)
+        .text("Inclination");
+
+    // y axis
+    svg.append("g")
+        .attr("class", "y axis")
+        .attr('id', "axis--y")
+        .call(yAxis);
+
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "1em")
+        .style("text-anchor", "end")
+        .text("Eccentricity");
+
+
+    //scatter.append("g")
+    //.attr("class", "brush")
+    //.call(brush);
+
+
+    function brushended() {
+
+        var s = d3.event.selection;
+        if (!s) {
+            if (!idleTimeout) return idleTimeout = setTimeout(idled, idleDelay);
+            x.domain(d3.extent(data, function (d) { return d.Inclination; })).nice();
+            y.domain(d3.extent(data, function (d) { return d.ecc; })).nice();
+        } else {
+            console.log("selected!")
+            x.domain([s[0][0], s[1][0]].map(x.invert, x));
+            y.domain([s[1][1], s[0][1]].map(y.invert, y));
+            scatter.select(".brush").call(brush.move, null);
+        }
+        zoom();
+        //console.log("zoomed")
+    }
+
+    function idled() {
+        idleTimeout = null;
+    }
+
+    function zoom() {
+
+        var t = scatter.transition().duration(750);
+        svg.select("#axis--x").transition(t).call(xAxis);
+        svg.select("#axis--y").transition(t).call(yAxis);
+        scatter.selectAll("circle").transition(t)
+            .attr("cx", function (d) { if (d.Country==Country && d.Users==Users){return x(d.Inclination)}
+                              else if(Country=="All") {if(d.Users==Users){return x(d.Inclination)}}
+                              else if(Users=="All") {if(d.Country==Country){return x(d.Inclination)}}})
+            .attr("cy", function (d) { if (d.Country==Country && d.Users==Users){return y(d.ecc)}
+                              else if(Country=="All") {if(d.Users==Users){return y(d.ecc)}}
+                              else if(Users=="All") {if(d.Country==Country){return y(d.ecc)}}});
+        //console.log("zoomed22")
+    }
+})
+}
+
+function start(){
+    
+    d3.csv('data_ellipse.csv', function (data) {
+
+        svg = d3.select("#scattersvg")
+
+     margin = { top: 20, right: 20, bottom: 30, left: 30 };
+    width = 900 - margin.left - margin.right,
+        height = 480 - margin.top - margin.bottom;
+
+     colors = ["blue", "green", "red", "black"]
+     colorScale = d3.scaleOrdinal()
+        .domain(["LEO", "MEO", "GEO", "Elliptical"])
+        //.domain(function (d) {return d.ClassOfOrbit})
+        .range(colors);
+
+     x = d3.scaleLinear()
+        .domain([
+            d3.min([0,d3.min(data,function (d) { return d.Inclination})]),
+            d3.max([0,d3.max(data,function (d) { return d.Inclination})])
+        ])
+        .range([0, width])
+        .nice();
+
+     y = d3.scaleLinear()
+        .domain([
+            d3.min([0,d3.min(data,function (d) { return d.ecc })]),
+            d3.max([0,d3.max(data,function (d) { return d.ecc })])
+        ])
+        .range([height, 0]);
+
+
+     tooltip = d3.select("#zoomableScatter").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+
+
+     xAxis = d3.axisBottom(x).ticks(12)
+        yAxis = d3.axisLeft(y).ticks(12 * height / width);
+
+
+    //var brush = d3.brush().extent([[0, 0], [width, height]]).on("end", brushended)
+    //.on("start", function(){d3.select("scatter").call(brush)}),
+    //  idleTimeout=100,
+    //idleDelay = 350;
+
+     svg = d3.select("#scattersvg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+     clip = svg.append("defs").append("svg:clipPath")
+        .attr("id", "clip")
+        .append("svg:rect")
+        .attr("width", width )
+        .attr("height", height )
+        .attr("x", 0)
+        .attr("y", 0);
+
+    //var xExtent = d3.extent(data, function (d) { return d.x; });
+    //var yExtent = d3.extent(data, function (d) { return d.y; });
+    //x.domain(d3.extent(data, function (d) { return d.x; })).nice();
+    //y.domain(d3.extent(data, function (d) { return d.y; })).nice();
+
+
+
+
+     scatter = svg.append("g")
+        .attr("id", "scatterplot")
+        .attr("clip-path", "url(#clip)");
+
+     brush = d3.brush().extent([[0, 0], [width, height]]).on("end", brushended),
         //.on("start", function(){d3.select("scatter").call(brush)}),
         idleTimeout,
         idleDelay = 350;
@@ -177,3 +385,5 @@ d3.csv('data_ellipse.csv', function (data) {
         //console.log("zoomed22")
     }
 })
+    
+}
