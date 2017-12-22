1bc287fb6721e3211cec9d50f4cfeb11837993c4
Shivam Miglani <shivam@Shivams-MacBook-Air.local>
2017-12-22 22:55:37 +0100

    Final final commit

--- js/helper_function.js
+++ js/helper_function.js
@@ -37,62 +37,8 @@ function locate(coord) {
     };
 }
 
-//Show the total orbit of the hovered over satellite
-function showEllipse(d, i, opacity) {
-    var satellite = i;
-    var duration = (opacity == 0) ? 2000 : 100; //If the opacity is zero slowly remove the orbit line
-
-    svg.selectAll(".sat")
-        .filter(function(d, i) { return i == satellite;})
-        .transition().duration(duration)
-        .style("stroke-opacity", opacity * 1.25);
-
-    //Select the orbit with the same index as the satellite
-    svg.selectAll(".orbit")
-        .filter(function(d, i) {console.log(i==satellite); return i == satellite;})
-        .transition().duration(duration)
-        .style("stroke-opacity", opacity)
-        .style("fill-opacity", opacity/3);
-
-}
-
 //Turn degrees into radians
 function toRadians (angle) { return angle * (Math.PI / 180);}
-
-
-//http://bl.ocks.org/mbostock/7555321
-function wrap(text, width) {
-    var text = d3.select(this[0][0]),
-        words = text.text().split(/\s+/).reverse(),
-        word,
-        line = [],
-        lineNumber = 0,
-        lineHeight = 1.4, // ems
-        y = text.attr("y"),
-        x = text.attr("x"),
-        dy = parseFloat(text.attr("dy")),
-        tspan = text.text(null).append("tspan").attr("x", x).attr("y", y).attr("dy", dy + "em");
-
-    while (word = words.pop()) {
-        line.push(word);
-        tspan.text(line.join(" "));
-        if (tspan.node().getComputedTextLength() > width) {
-            line.pop();
-            tspan.text(line.join(" "));
-            line = [word];
-            tspan = text.append("tspan").attr("x", x).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
-        }
-    }
-};
-
-//Taken from https://groups.google.com/forum/#!msg/d3-js/WC_7Xi6VV50/j1HK0vIWI-EJ
-//Calls a function only after the total transition ends
-function endall(transition, callback) {
-    var n = 0;
-    transition
-        .each(function() { ++n; })
-        .each("end", function() { if (!--n) callback.apply(this, arguments); });
-}
 
 //http://stackoverflow.com/questions/16265123/resize-svg-when-window-is-resized-in-d3-js
 function updateWindow(){
